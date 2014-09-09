fa01d805ab9a7f7df7946784e1975c280237c03b
student <student@R150-S3.local>
2014-09-09 15:42:44 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,6 @@
+<html>
+<head><title>Home Page</title></head>
+<body>
+<header>
+This is the header of the Website
+</header>
---
+++ week3.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+* var title string - The title
+* var before string - tag before the title
+* var after string - tag after the title
+* return- echo title
+*/
+
+$title = 'Week 3 Assignments';
+
+$before = '<h3>';
+$after = '</h3>';
+
+$title = $before + $title + $after;
+
+ echo $title;
+
+$num1 = 3;
+$num2 = 5;
+
+$total1 = $num1 + $num2;
+
+$total2 = $num1 . $num2;
+
+// echo $total2;
+
+?>
+
+<h4>Week 3</h4>
